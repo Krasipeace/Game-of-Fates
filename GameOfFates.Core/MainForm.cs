d71214287cb80eb409c7b671beeda67c23f5164c
Krasimir Dramaliev <krasipeace@abv.bg>
2024-05-20 17:29:15 +0300

    repo init

---
+++ GameOfFates.Core/MainForm.cs
@@ -0,0 +1,26 @@
+﻿namespace GameOfFates.Core
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            CenterToScreen();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
