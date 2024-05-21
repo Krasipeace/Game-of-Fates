2031f93b0573e68db584af19b8e77657f4b1c82f
Krasimir Dramaliev <krasipeace@abv.bg>
2024-05-21 18:09:41 +0300

    add new sound, fix the bug where bg-sound stops, when buttons are clicked

--- GameOfFates.Core/MainForm.cs
+++ GameOfFates.Core/MainForm.cs
@@ -5,6 +5,8 @@
     using System.ComponentModel;
     using System.Data;
     using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
     using System.Linq;
     using System.Text;
     using System.Threading.Tasks;
@@ -20,27 +22,24 @@
             CenterToScreen();
             backgroundSound = new System.Media.SoundPlayer
             {
-                SoundLocation = System.IO.Path.Combine(Application.StartupPath, @"..\..\Resources\Sounds\background.wav")
+                SoundLocation = Path.Combine(Application.StartupPath, @"..\..\Resources\Sounds\background.wav")
             };
             backgroundSound.Load();
+            backgroundSound.PlayLooping();
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
-            backgroundSound.PlayLooping();
             PictureBoxBeforeAsking(sender, e);
             PictureBoxAfterAsking(sender, e);
-        }
-
-        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
-        {
-
+            predictButton.Enabled = false;
         }
 
         private void PictureBoxBeforeAsking(object sender, EventArgs e)
         {
-            string imagePath = System.IO.Path.Combine(Application.StartupPath, @"..\..\Resources\Images\beforeAsking.jpg");
+            string imagePath = Path.Combine(Application.StartupPath, @"..\..\Resources\Images\beforeAsking.jpg");
             Image originalImage = Image.FromFile(imagePath);
+
             Image resizedImage = ResizeImage(originalImage, 480, 480);
 
             picBeforeAsking.Image = resizedImage;
@@ -48,11 +47,96 @@
 
         private void PictureBoxAfterAsking(object sender, EventArgs e)
         {
-            string imagePath = System.IO.Path.Combine(Application.StartupPath, @"..\..\Resources\Images\afterAsking.jpg");
+            string imagePath = Path.Combine(Application.StartupPath, @"..\..\Resources\Images\afterAsking.jpg");
             Image originalImage = Image.FromFile(imagePath);
+
             Image resizedImage = ResizeImage(originalImage, 480, 480);
 
             picAfterAsking.Image = resizedImage;
+        }
+
+        private void LabelAskMe(object sender, EventArgs e)
+        {
+        }
+
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            predictButton.Enabled = !string.IsNullOrWhiteSpace(userInputTextBox.Text);
+        }
+
+        private async void PredictClick(object sender, EventArgs e)
+        {
+            predictButton.Visible = false;
+            askMeAnythingLabel.Visible = false;
+            picAfterAsking.Visible = true;
+            resetButton.Visible = true;
+            fateOutputTextBox.Visible = true;
+
+            string soundPath = @"..\..\Resources\Sounds\predictSound.wav";
+            await PlaySoundEffectAsync(soundPath);
+            backgroundSound.Load();
+            backgroundSound.PlayLooping();
+        }
+
+        private async void ResetPrediction(object sender, EventArgs e)
+        {
+            predictButton.Visible = true;
+            askMeAnythingLabel.Visible = true;
+            picAfterAsking.Visible = false;
+            resetButton.Visible = false;
+            fateOutputTextBox.Text = string.Empty;
+            fateOutputTextBox.Visible = false;
+            userInputTextBox.Text = string.Empty;
+
+            string soundPath = @"..\..\Resources\Sounds\resetSound.wav";
+            await PlaySoundEffectAsync(soundPath);
+            backgroundSound.Load();
+            backgroundSound.PlayLooping();
+        }
+
+        private void FateAnswer_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AboutLinkLabel_Click(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/Krasipeace/Game-of-Fates");
+        }
+
+        private void ShareFateToSocialNetwork()
+        {
+            Bitmap screenshot = new Bitmap(picAfterAsking.Width, picAfterAsking.Height);
+            using (Graphics graphics = Graphics.FromImage(screenshot))
+            {
+                graphics.CopyFromScreen(
+                    picAfterAsking.PointToScreen(Point.Empty),
+                    Point.Empty,
+                    picAfterAsking.Size
+                );
+            }
+
+            string downloadsFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            string tempFilePath = Path.Combine(downloadsFolder, "screenshot.png");
+            screenshot.Save(tempFilePath, ImageFormat.Png);
+
+            ShareToSocialNetwork(tempFilePath);
+
+            File.Delete(tempFilePath);
+        }
+
+        private void ShareToSocialNetwork(string filePath)
+        {
+
+        }
+
+        #region ExtensionMethods
+        private async Task PlaySoundEffectAsync(string soundPath)
+        {
+            using (var soundPlayer = new System.Media.SoundPlayer(soundPath))
+            {
+                await Task.Run(() => soundPlayer.PlaySync());
+            }
         }
 
         private Image ResizeImage(Image image, int width, int height)
@@ -65,50 +149,6 @@
 
             return resizedImage;
         }
-
-        private void LabelAskMe(object sender, EventArgs e)
-        {
-
-        }
-
-        private void TextBox_TextChanged(object sender, EventArgs e)
-        {
-            predictButton.Enabled = !string.IsNullOrWhiteSpace(userInputTextBox.Text);
-        }
-
-        private void PredictClick(object sender, EventArgs e)
-        {
-            predictButton.Visible = false;
-            askMeAnythingLabel.Visible = false;
-            picAfterAsking.Visible = true;
-            resetButton.Visible = true;
-            fateOutputTextBox.Visible = true;
-            System.Media.SoundPlayer predictSound = new System.Media.SoundPlayer
-            {
-                SoundLocation = System.IO.Path.Combine(Application.StartupPath, @"..\..\Resources\Sounds\predictSound.wav")
-            };
-            predictSound.Load();
-            predictSound.Play();
-        }
-
-        private void ResetPrediction(object sender, EventArgs e)
-        {
-            predictButton.Visible = true;
-            askMeAnythingLabel.Visible = true;
-            picAfterAsking.Visible = false;
-            resetButton.Visible = false;
-            fateOutputTextBox.Text = string.Empty;
-            fateOutputTextBox.Visible = false;
-        }
-
-        private void FateAnswer_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void AboutLinkLabel_Click(object sender, LinkLabelLinkClickedEventArgs e)
-        {
-            System.Diagnostics.Process.Start("https://github.com/Krasipeace/Game-of-Fates");
-        }
+        #endregion
     }
 }
--- readme.md
+++ readme.md
@@ -1,13 +1,16 @@
+
 # Game of Fates
 
 .Net Winforms Desktop App, which generates generic answers based on user input. It has functionality to share your generated fate with social networks.
 
-## Demo
+## Demo/Screenshots
 
 
 
-### 3rd party Resources 
+### 3rd-party Resources 
 
 - Background Music: <a href="https://pixabay.com/users/gregoryallenbrown-6307922/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=189391">Gregory Brown</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=189391">Pixabay</a>
-- Sound Effects: <a href="https://pixabay.com/users/floraphonic-38928062/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=174462">floraphonic</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=174462">Pixabay</a>
+- Sound Effects: 
+  - <a href="https://pixabay.com/users/floraphonic-38928062/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=174462">floraphonic</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=174462">Pixabay</a>
+  - <a href="https://pixabay.com/bg/users/universfield-28281460/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=144746">UNIVERSFIELD</a> from <a href="https://pixabay.com/sound-effects//?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=144746">Pixabay</a>
 - Images: [Copilot](https://copilot.microsoft.com/?form=MT00D9)
